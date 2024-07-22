323a62e338abf066b31c46dd2b610c7dcc096c07
madcapnawapon <114137572+madcapnawapon@users.noreply.github.com>
2024-07-22 23:19:22 +0700

    Add files via upload

---
+++ ledmatrix711.ino
@@ -0,0 +1,30 @@
+#include "Arduino_LED_Matrix.h"
+
+ArduinoLEDMatrix matrix;
+
+void setup() {
+  Serial.begin(115200);
+  matrix.begin();
+}
+
+uint8_t frame[8][12] = {
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+};
+
+void loop(){
+  frame[0][0]=1;
+  frame[7][11]=0;
+  matrix.renderBitmap(frame, 8, 12);
+  delay(1000);
+  frame[0][0]=0;
+  frame[7][11]=1;
+  matrix.renderBitmap(frame, 8, 12);
+  delay(1000);
+}
